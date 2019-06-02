e07d203892c8420ad7224e0e304f6661a04f2c73
RoxanaCortez <00502211@uca.edu.sv>
2019-06-01 23:05:14 -0600

    BkballRoomDatabase creado

---
+++ app/src/main/java/com/roxyapps/roxana/parcial1pdm/BkballRoomDatabase.kt
@@ -0,0 +1,35 @@
+package com.roxyapps.roxana.parcial1pdm
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.roxyapps.roxana.parcial1pdm.Dao.BkballEquipoDao
+import com.roxyapps.roxana.parcial1pdm.Entity.BkballEquipo
+
+@Database(entities = [BkballEquipo::class], version = 1, exportSchema = false)
+abstract class BkballRoomDatabase : RoomDatabase() {
+
+    abstract fun BkballEquipoDao() : BkballEquipoDao
+
+    companion object {
+        @Volatile
+        private  var INSTANCE : BkballRoomDatabase? = null
+
+        fun getInstance(
+            context: Context
+        ): BkballRoomDatabase {
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room
+                    .databaseBuilder(context.applicationContext, BkballRoomDatabase::class.java, "Equipos")
+                    .build()
+                    INSTANCE=instance
+                    return instance
+            }
+        }
+    }
+}
