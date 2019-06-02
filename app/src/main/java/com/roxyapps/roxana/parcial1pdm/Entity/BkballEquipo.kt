f5e7022b33a2a6dee92ebced169420274cb45532
RoxanaCortez <00502211@uca.edu.sv>
2019-06-01 22:23:29 -0600

    Entidad BkbEquipo creada

--- app/src/main/java/com/roxyapps/roxana/parcial1pdm/Entity/BkballEquipo.kt
+++ app/src/main/java/com/roxyapps/roxana/parcial1pdm/Entity/BkballEquipo.kt
@@ -1,2 +1,23 @@
 package com.roxyapps.roxana.parcial1pdm.Entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Date
+import java.sql.Time
+
+@Entity(tableName = "BkballEquipo")
+data class BkballEquipo (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") val id:Int,
+
+    @ColumnInfo(name = "PrimerEquipo") val PrimerEquipo : String,
+    @ColumnInfo(name = "PrimerPuntaje") val PrimerPuntaje : String,
+
+    @ColumnInfo(name = "SegundoEquipo") val SegundoEquipo : String,
+    @ColumnInfo(name = "SegundoPuntaje") val SegundoPuntaje : String,
+
+    @ColumnInfo(name = "Fecha") val Fecha : Date,
+    @ColumnInfo(name = "Hora") val Hora : Time
+
+)
